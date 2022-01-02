014af6e3eda2ba6127e6d60d098342cb611c3074
Yoni Romm <yonik12@gmail.com>
2022-01-02 21:46:35 +0200

    added comments and in bonus section normed the y axis

--- bonus.py
+++ bonus.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 from matplotlib import patches
 from kmeans_pp import k_means_initialization
 
+# we load the iris database and calculating for k = 1 to 10 the average dispersion
+# we are using KMeans algorithm with the kmeans++ initialization algorithm that we implemented
+# we are ploting all the result in a chart
 if __name__ == '__main__':
     iris = datasets.load_iris()
     iris_data = iris.data
@@ -16,11 +19,12 @@ if __name__ == '__main__':
         kmeans = KMeans(n_clusters=k,init=initial_centroids,random_state=0,n_init=1).fit(iris_data)
         inertia = kmeans.inertia_
         average_dispersion.append(inertia)
+    average_dispersion = [average_dispersion[i]/len(iris_data) for i in range(len(average_dispersion))]
 
     ax = plt.gca()
     ax.cla() # clear things for fresh plot
     plt.plot(k_array, average_dispersion)
-    circle = patches.Ellipse((2, 150), 2,100, color='b', fill=False)
+    circle = patches.Ellipse((2, 1), 1,0.5, color='b', fill=False)
     plt.title('Elbow Method')
     plt.xlabel('K')
     plt.ylabel('Average Dispersion')
